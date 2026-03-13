f74a4ef9452cccd05c6d46231fc701ae39107992
lopezzuluagaj3-colab <lopezzuluagaj3@gmail.com>
2026-03-12 19:40:08 -0500

    correcion de errores

--- js/services/apiConfig.js
+++ js/services/apiConfig.js
@@ -3,4 +3,4 @@ export const API_ORIGIN =
     ? window.location.origin
     : `${window.location.protocol}//${window.location.hostname}:3000`;
 
-export const API_BASE_URL = `${API_ORIGIN}/api`;
+export const API_BASE_URL = API_ORIGIN;
